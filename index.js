5c9b303a2fbcf6ad0f66292b3c49a1d27eaea52e
Brian Leathem <bleathem@gmail.com>
2016-12-23 15:20:56 -0800

    Switched to using express

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 'use strict';
 
-const Hapi = require('hapi');
+const express = require('express')
+const app = express()
 
-const server = new Hapi.Server();
-server.connection({ port: 8080 });
+app.get('/', function (req, res) {
+  res.send('Hello world Express app!')
+})
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
